54c1e314e3228a82790eb717e7e01c6181ca4b5c
Z1HA0WU <wzh3278664250@outlook.com>
2025-12-03 10:50:09 +0800

    Add script to download datasets using gdown

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+mkdir data
+cd data
+gdown 1AQjUn3m010N_i6-sfD690W7mZq2RTwJz # ImageNet (4.62 GB)
+gdown 1yXD9XqBxEJnJxcfnnduNK6nHHUU3iX_6 # CIFAR-100 (0.01 GB)
+gdown 119k7PE1l72fg5Rpez5brIOn28BwClqv2 # Places365 (0.54 GB)
+gdown 1BUlQZhS_5x2LJpyxCGI1IkmRrkvmRD88 # iNaturalist (6.72 GB)
+cd .. 
